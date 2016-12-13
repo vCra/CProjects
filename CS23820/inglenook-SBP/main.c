07149ddbfe76c968b50ff7987c29f3d3f82b1dcf
vCra <aaw13@aber.ac.uk>
2016-12-13 15:15:21 +0000

    In an okay state

--- CS23820/inglenook-SBP/main.c
+++ CS23820/inglenook-SBP/main.c
@@ -13,7 +13,7 @@ typedef struct {
     char status;
 } point;
 
-typedef struct siding{
+typedef struct {
     int Capacity;
     int Stored; //the ammount of wagons currently in the
 } siding;
@@ -21,27 +21,48 @@ typedef struct {
     siding Sidings[4];
     point Points[2];
 } track;
+char* getStatus(int point, track* Track){
+    if (Track->Points[point].status=='N') {
+        return "NORMAL";
+    }
+    else if (Track->Points[point].status=='R') {
+        return "REVERSE";
+    }
+}
+void printStatus(track* Track, int status, int socket){
+    char str[80];
+    strcpy(str, "STATUS ");
+    //strcat(str, (__STRING(status)));
+    strcat(str, getStatus(0, Track));
+    strcat(str, " ");
+    strcat(str, getStatus(1, Track));
+    strcat(str, "\n");
+    printf(str);
+    write(socket,str,80);
 
-void movePoints(int siding, track Track){
+}
+void movePoints(int siding, track* Track){
     switch(siding){
         case 1:
-            Track.Points[0].status='R';
+            Track->Points[0].status='R';
             break;
         case 2:
-            Track.Points[0].status='N';
-            Track.Points[1].status='R';
+            Track->Points[0].status='N';
+            Track->Points[1].status='R';
             break;
         case 3:
-            Track.Points[0].status='N';
-            Track.Points[1].status='N';
+            Track->Points[0].status='N';
+            Track->Points[1].status='N';
+            break;
+        default:
             break;
     }
 }
-int trackPut(int sidingNumber, int numberWagons, track Track){
-    if(Track.Sidings[sidingNumber].Stored + numberWagons > Track.Sidings[sidingNumber].Capacity){
+int trackPut(int sidingNumber, int numberWagons, track* Track){
+    if(Track->Sidings[sidingNumber].Stored + numberWagons > Track->Sidings[sidingNumber].Capacity){
         return -1;
     }
-    else if(Track.Sidings[0].Stored < numberWagons){
+    else if(Track->Sidings[0].Stored < numberWagons){
         return -2;
     }
     else if((sidingNumber < 0)||(sidingNumber> 3)){
@@ -50,33 +71,41 @@ int trackPut(int sidingNumber, int numberWagons, track Track){
     else{
         movePoints(sidingNumber, Track);
         return numberWagons;
-
     }
 };
-int trackTake(int sidingNumber, int numberWagons, track Track){
-    if(Track.Sidings[0].Stored+numberWagons > Track.Sidings[0].Capacity){
+int trackTake(int sidingNumber, int numberWagons, track* Track){
+    if(Track->Sidings[0].Stored+numberWagons > Track->Sidings[0].Capacity){
         return -1;
     }
-    else if(Track.Sidings[sidingNumber].Stored < numberWagons){
+    else if(Track->Sidings[sidingNumber].Stored < numberWagons){
         return -2;
     }
     else if (sidingNumber <3 || sidingNumber>3){
         return -3;
     }
+    else{
+        movePoints(sidingNumber, Track);
+        return numberWagons;
+    }
 };
-void init(track Track){
-    Track.Sidings[0].Capacity=3;
-    Track.Sidings[1].Capacity=5;
-    Track.Sidings[2].Capacity=3;
-    Track.Sidings[3].Capacity=3;
-}
-int trackLoad(int s1, int s2, int s3, track Track){
-    Track.Sidings[1].Stored=s1;
-    Track.Sidings[2].Stored=s2;
-    Track.Sidings[3].Stored=s3;
+int init(track* Track){
+    Track->Sidings[0].Capacity=3;
+    Track->Sidings[1].Capacity=5;
+    Track->Sidings[2].Capacity=3;
+    Track->Sidings[3].Capacity=3;
+    Track->Points[0].status='N';
+    Track->Points[1].status='N';
+    return 1;
 };
 
-int processSocket (int sock, track track){
+int trackLoad(int s1, int s2, int s3, track* currentTrack){
+    currentTrack->Sidings[1].Stored=s1;
+    currentTrack->Sidings[2].Stored=s2;
+    currentTrack->Sidings[3].Stored=s3;
+    printf("Added wagons to track!");
+};
+
+int processSocket (int sock, track Track){
     int n;
     char buffer[256];
     bzero(buffer,256);
@@ -91,24 +120,33 @@ int processSocket (int sock, track track){
     int s; //For status
 
     a1 = strtok(buffer, " "); //split it up
+    printf("Command is %s\n",a1);
+    printf("-------------\n");
     if (a1=="put"){
 
-        a2 = atoi(atoi(a2));
-        a3 = atoi(atoi(a3));
-        s = trackPut(a2, a3, track);
+        a2 = atoi(strtok(NULL, " "));
+        a3 = atoi(strtok(NULL, " "));
+        s = trackPut(a2, a3, &Track);
+        printStatus(&Track, s, sock);
+
     }
     else if (a1=="take"){
-        a2 = atoi(atoi(a2));
-        a3 = atoi(atoi(a3));
-        s = trackTake(a2,a3, track);
+        a2 = atoi(strtok(NULL, " "));
+        a3 = atoi(strtok(NULL, " "));
+        s = trackTake(a2,a3, &Track);
+        printStatus(&Track, s, sock);
+
     }
     else if (a1=="load"){
-        a2 = atoi(atoi(a2));
-        a3 = atoi(atoi(a3));
-        a4 = atoi(atoi(a4));
-        s = trackLoad(a2,a3,a4, track);
+        a2 = atoi(strtok(NULL, " "));
+        a3 = atoi(strtok(NULL, " "));
+        a4 = atoi(strtok(NULL, " "));
+        s = trackLoad(a2,a3,a4, &Track);
+        printStatus(&Track, s, sock);
+
     }
-    n = write(sock,s,20);
+    printStatus(&Track, 4, sock);
+    printf("%i", s);
     if (n < 0) {
         perror("ERROR writing to socket");
         exit(1);
@@ -122,7 +160,10 @@ int main( int argc, char *argv[] ) {
     struct sockaddr_in serv_addr, cli_addr;
     int n, pid;
 
+    //Setup track
     track Track;
+    init(&Track);
+
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -168,15 +209,15 @@ int main( int argc, char *argv[] ) {
             showWelcome(newsockfd);
             int stop=0;
             while(stop==0){
+
                 stop = processSocket(newsockfd, Track);
             }
-
+            write(newsockfd,"Goodbye\n",23);
             exit(0);
         }
         else {
             close(newsockfd);
         }
-        write(newsockfd,"Goodbye\n",23);
 
     } /* end of while */
 }
