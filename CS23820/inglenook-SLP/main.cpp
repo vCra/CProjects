6b38dc10053ec45afb0ac441652e1cfc7c03c29c
vCra <aaw13@aber.ac.uk>
2016-12-13 23:48:54 +0000

    Started working on SLP

--- CS23820/inglenook-SLP/main.cpp
+++ CS23820/inglenook-SLP/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+
 void printMenu(){
     std::cout << "Inglenook Sidings aaw13" << std::endl;
     std::cout << "1. Import Wagons" << std::endl;
@@ -11,33 +13,73 @@ void printMenu(){
 }
 
 int getInput(){
-    std::cout << "Option: ";
+    std::cout << "input: ";
     int input;
     std::cin >> input;
     return input;
 }
 void moveWagons(){
-
+    std::cout << "##         Move Wagons        ##" << std::endl;
+    std::cout << "1. Move from Siding to HeadShunt" << std::endl;
+    std::cout << "2. Move from HeadShunt to Siding" << std::endl;
+    int input = getInput();
+    switch(input){
+        case 1 : {
+            std::cout << "Enter the siding number" << std::endl;
+            int siding = getInput();
+        }
+        case 2 : {
+            std::cout << "Enter the siding number" << std::endl;
+            int siding = getInput();
+        }
+        default : std::cout << "!!! Unknown Option !!!" << std::endl;
+    }
 }
 void printSetup(){
 
 }
 void importSidings(){
-
+    std::cout << "## Import Sidings ##" << std::endl;
+    std::cout << "1. Import from file" << std::endl;
+    std::cout << "2. Import manually " << std::endl;
+    int input = getInput();
+    switch(input){
+        case 1 : {
+            //Import from file
+        }
+        case 2 : {
+            //Import Manually
+        }
+        default : std::cout << "!!! Unknown Option !!!" << std::endl;
+    }
 }
 void importWagons(){
-
+    std::cout << "## Import Wagons ##" << std::endl;
+    std::cout << "1. Import from file" << std::endl;
+    std::cout << "2. Import manually " << std::endl;
+    int input = getInput();
+    switch(input){
+        case 1 : {
+            //Import from file
+        }
+        case 2 : {
+            //Import Manually
+        }
+        default : std::cout << "!!! Unknown Option !!!" << std::endl;
+    }
 }
 void run(){
     printMenu();
     int input = getInput();
     while (input != 9){
         switch(input){
-            case 1 :importWagons();
-            case 2 : importSidings();
-            case 3 : printSetup();
-            case 4 : moveWagons();
+            case 1 : {importWagons(); break;}
+            case 2 : {importSidings(); break;}
+            case 3 : {printSetup(); break;}
+            case 4 : {moveWagons(); break;}
+            default: std::cout << "Not valid!" << std::endl;
         }
+        printMenu();
         input = getInput();
     }
 }
